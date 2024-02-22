9659605a39c4246a7ef8de0ff09fb426921fa0c6
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-02-22 15:57:45 -0500

    Adding Part 3 to show links.

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ var menuLinks = [
     { text: 'account', href: '/account' },
   ];
 
+// Part 1:
 const mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
 mainEl.classList.add("flex-ctr");
 
+// Part 2:
 const topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add("flex-around");
+
+// Part 3:
+
+const nav = document.querySelector('nav')
+
+for (var i = 0; i < menuLinks.length; i++) {
+    var link = menuLinks[i];
+    var menuItem = document.createElement('a');
+    menuItem.textContent = link.text;
+    menuItem.href = link.href;
+    nav.appendChild(menuItem);
+}
 
