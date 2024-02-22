bab3509575d1dd05cd9f363f9091842a545b69c1
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-02-22 11:30:56 -0500

    Part 1 completed.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-let mainEL = document.querySelector('main');
+const mainEl = document.querySelector('main');
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+
+mainEl.classList.add("flex-ctr");
